feaa35ec48ad7c8799659d511caba2b5c06c0d98
horairahsaleem <horairahsaleem7864@gmail.com>
2023-04-11 19:53:55 +0500

    feat:pushing lab task 1 2

---
+++ web/lab_task_project_1_2/js/myscript.js
@@ -0,0 +1,11 @@
+// console.log('cdl');
+// function validateForm() {
+//   console.log('here');
+//   let name = document.getElementsByName("myname")[0].value;
+//   console.log(name);  
+
+//     if (name == "") {
+//       alert("Must enter the name ");
+//       return false;
+//     }
+//   }
